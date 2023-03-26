22c9f15d4ec03b3d144b10f67d63faba26fcc0b5
dyhalmeida <dyhalmeida@gmail.com>
2023-03-26 20:23:23 -0300

    Add transactions table and styles to Transactions page

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,46 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary/Summary";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
     return (
         <div>
             <Header />
             <Summary />
+            <TransactionsContainer>
+              <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td>Desenvolvimento de Software</td>
+                            <td>
+                                <PriceHighlight variant="income">
+                                    R$ 14.000,00
+                                </PriceHighlight>
+                            </td>
+                            <td>
+                                Entrada
+                            </td>
+                            <td>
+                                26/03/2022
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Internet</td>
+                            <td>
+                                <PriceHighlight variant="outcome">
+                                    - R$ 130,00
+                                </PriceHighlight>
+                            </td>
+                            <td>
+                                Saída
+                            </td>
+                            <td>
+                                26/03/2022
+                            </td>
+                        </tr>
+                    </tbody>
+              </TransactionsTable>
+            </TransactionsContainer>
         </div>
     )
 }
