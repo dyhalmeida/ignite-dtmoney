07e4ea5e191ebb18163930638107d3cffe9c1c9e
dyhalmeida <dyhalmeida@gmail.com>
2023-03-27 18:05:26 -0300

    Add search functionality to transactions page using react-hook-form and zod validation

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,13 +1,40 @@
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { MagnifyingGlass } from "phosphor-react";
 import { SearchFormContainer } from "./styles";
 
-export function SearchForm() {
-  return (
-    <SearchFormContainer>
-      
-      <input type="text" placeholder="Busque por transações" />
+const searchFormSchema = z.object({
+  query: z.string(),
+})
 
-      <button type="submit">
+type TSearchFormInputs = z.infer<typeof searchFormSchema>
+
+export function SearchForm() {
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<TSearchFormInputs>({
+    resolver: zodResolver(searchFormSchema),
+  })
+
+  function handleSearchTransactions(data: TSearchFormInputs) {
+    console.log(data)
+  }
+
+
+  return (
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      
+      <input
+        type="text"
+        placeholder="Busque por transações"
+        {...register('query')}
+      />
+
+      <button type="submit" disabled={isSubmitting}>
         <MagnifyingGlass size={20} />
         Buscar
       </button>
