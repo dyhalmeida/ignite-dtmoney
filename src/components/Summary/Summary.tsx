f0a962191ebfdd5112e06a017df8f996cc859444
dyhalmeida <dyhalmeida@gmail.com>
2023-03-27 17:28:03 -0300

    Add useTransactions hook to Summary component and calculate summary of transactions in real time

--- src/components/Summary/Summary.tsx
+++ src/components/Summary/Summary.tsx
@@ -1,7 +1,27 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
+import { useTransactions } from '../../hooks/useTransactions'
 import { SummaryCard, SummaryContainer } from './styles'
 
 export function Summary() {
+
+  const { transactions } = useTransactions()
+
+  const summary = transactions.reduce((acc, transaction) => {
+
+    if (transaction.type === 'income') {
+      acc.income += transaction.price
+      acc.total += transaction.price
+    } else {
+      acc.outcome += transaction.price
+      acc.total -= transaction.price
+    }
+
+    return acc
+  }, {
+    income: 0,
+    outcome: 0,
+    total: 0
+  })
 
   return (
     <SummaryContainer>
@@ -11,7 +31,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
 
-        <strong>R$ 14.000,00</strong>
+        <strong>{summary.income}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -20,7 +40,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
 
-        <strong>R$ 14.000,00</strong>
+        <strong>{summary.outcome}</strong>
       </SummaryCard>
 
       <SummaryCard variant="green">
@@ -29,7 +49,7 @@ export function Summary() {
           <CurrencyDollar size={32} color="#fff" />
         </header>
 
-        <strong>R$ 14.000,00</strong>
+        <strong>{summary.total}</strong>
       </SummaryCard>
     </SummaryContainer>
   )
